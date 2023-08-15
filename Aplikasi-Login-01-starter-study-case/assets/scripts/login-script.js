bcba0d8847e61bb5e9fb54c45c9303842e174d5a
beryllpradanaramadhan17@gmail.com <you@example.com>
2023-08-15 22:11:40 +0700

    Latihan pembuatan aplikasi login Dicoding

---
+++ Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
@@ -0,0 +1,18 @@
+const loginFormElement = document.querySelector("#loginForm");
+const inputEmailElement = document.querySelector("#inputEmail");
+const inputPasswordElement = document.querySelector("#inputPassword");
+
+const expectedEmail = "admin@dicoding.com";
+const expectedPassword = "superpassword";
+
+loginFormElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+
+  if (email == expectedEmail && password == expectedPassword) {
+    goToHome();
+  } else {
+    showPopUp();
+  }
+});
